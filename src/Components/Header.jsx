d2e4e6e0b01c7dcd8ad1f552af59b3469e7d7f7b
MamounE1 <mamoun.el100@gmail.com>
2025-09-12 17:34:26 -0500

    Added header styling, and created additional components

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,10 @@
+import "../index.css"
+
 export default function Header(){
     return (
         <>
             <header>
-                <h1>MyBookFinder</h1>
+                <h1 className="title">MyBookFinder</h1>
             </header>
         </>
     )
