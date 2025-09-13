9b23f26741a745afe663733f2e89271e9a03f78a
MamounE1 <mamoun.el100@gmail.com>
2025-09-13 09:53:18 -0500

    Added useState and useEffect to fetch data from the google books API using a static query.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { useState, useEffect } from "react"
 import BookList from "./Components/BookList/BookList.jsx"
 import Header from "./Components/Header/Header.jsx"
 import "./index.css"
-import { fakeBooks } from "./fakeData.js"
 
 export default function App() {
+  const [books, setBooks] = useState([])
+  useEffect(() => {
+    fetch("https://www.googleapis.com/books/v1/volumes?q=harry+potter&startIndex=10&maxResults=40")
+      .then(res => res.json())
+      .then(data => setBooks(data.items || []))
+  }, [])
+  
   return (
     <>
       <Header />
-      <BookList books={fakeBooks}/>
+      <BookList books={books}/>
     </>
   )
 }
