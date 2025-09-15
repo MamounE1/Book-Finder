b02ebb5c8cac0d4fe123261ab622a05cc3b4f4e6
MamounE1 <mamoun.el100@gmail.com>
2025-09-15 17:57:01 -0500

    Updated app name

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,7 @@ import "./Header.css"
 export default function Header(){
     return (
         <header>
-            <h1 className="title">MyBookFinder</h1>
+            <h1 className="title">LitScout</h1>
         </header>
     )
 }
